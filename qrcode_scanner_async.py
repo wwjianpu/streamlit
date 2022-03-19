4adb770ef200eb85c4bd014fa058d438303f5bc6
wwjianpu <wwjianpu@gmail.com>
2022-03-19 22:21:57 +0800

    corp_img

--- qrcode_scanner_async.py
+++ qrcode_scanner_async.py
@@ -11,6 +11,8 @@ RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
+
+
 def main():
     class VideoProcessor(VideoProcessorBase):
 
@@ -18,26 +20,61 @@ def main():
             self.d = cv2.QRCodeDetector()
             self.startcapture = False
             self.captured = False
+            self.crop = False
+            self.crop_border = False
             self.value = ""
 
-        async def recv_queued(self, frames: List[av.AudioFrame]) -> List[av.VideoFrame]: 
+        def get_rectangle_points(self, nparr, border=False):
+            ''' return start point & end point astype(int)'''
+            if nparr is not None: 
+                if nparr.size > 1:
+                    if border: b = 40
+                    else: b = 0
+                
+                    n_pts = (int) (nparr.size / 2)
+                    nparr = nparr.astype(int)
+                    start_point = (np.min(nparr.reshape(n_pts,2)[:, 0]) - b, np.min(nparr.reshape(n_pts,2)[:, 1]) - b)
+                    end_point = (np.max(nparr.reshape(n_pts,2)[:, 0]) + b, np.max(nparr.reshape(n_pts,2)[:, 1]) + b)
+
+                    return start_point, end_point
+            
+            return (0,0), (0,0)
+
+        async def recv_queued(self, frames: List[av.VideoFrame]) -> List[av.VideoFrame]: 
 
             rtn_frames = []
 
             for frame in frames:
+                
                 img = frame.to_ndarray(format="bgr24")
                 val, points = self.d.detect(img)
 
                 if val:
-                    img = cv2.polylines(img, [points.astype(int)], True, (0,255,0), 2)
+                    start_pt, end_pt = self.get_rectangle_points(points, self.crop_border)
+                    # area to be cropped
+                    x,y,yh,xw = max(start_pt[0],0),max(start_pt[1],0), min(end_pt[1], frame.height), min(end_pt[0], frame.width)
 
                     if self.startcapture:
-                        qrcode_val, points, straight_qrcode = self.d.detectAndDecode(img)
+                        # qrcode_val, crop_points, straight_qrcode = self.d.detectAndDecode(img)
+                        
+                        if self.crop:
+                            crop_img = img[y:yh, x:xw]    
+                            qrcode_val, crop_points, straight_qrcode = self.d.detectAndDecode(crop_img)
+
+                            rows, cols, channels = crop_img.shape
+                            img[0:rows, 0:cols] = crop_img
+                        else:
+                            qrcode_val, crop_points, straight_qrcode = self.d.detectAndDecode(img)
+
                         if (qrcode_val):
                             self.value = qrcode_val
                             self.startcapture = False
                             self.captured = True
                             #print("capture OFF ", qrcode_val)
+                    img = cv2.polylines(img, [points.astype(int)], True, (0,255,0), 2)
+                    
+                    
+                    img = cv2.rectangle(img, start_pt, end_pt, (255,0,255), 2)
 
                 # get single frame for return only
                 rtn_frames.append(av.VideoFrame.from_ndarray(img, format="bgr24"))
@@ -57,7 +94,7 @@ def main():
                 "frameRate": {"ideal": frame_rate, "max": 15}, 
                 "width": {"min": 800, "ideal": 1280, "max": 1920 }, },            
             "audio": False, },
-        rtc_configuration=RTC_CONFIGURATION, 
+        #rtc_configuration=RTC_CONFIGURATION, 
         async_processing=True,
     )
     
@@ -71,9 +108,17 @@ def main():
         st.session_state['qrcode_value'] = ""
     
     
+    def option_changed():
+        if ctx.video_processor:
+            ctx.video_processor.crop = st.session_state.crop
+            ctx.video_processor.crop_border = st.session_state.crop_border
+
     # sidebar
     time_limit = st.sidebar.number_input("time limit (in second) for capturing ", min_value=1, max_value=300,value=20)
     time_limit = time_limit * 10
+    st.sidebar.checkbox("cropped qrcode for detect", value=False, key="crop", on_change=option_changed)
+    st.sidebar.checkbox("crop with border", value = False, key="crop_border", on_change=option_changed)
+        
 
     with st.container():
 
@@ -101,9 +146,14 @@ def main():
 
     if ctx.video_processor:
         ctx.video_processor.startcapture = st.session_state.cap
+        ctx.video_processor.crop = st.session_state.crop
+        ctx.video_processor.crop_border = st.session_state.crop_border
+
         st.session_state['start_time'] = datetime.now().strftime("%H:%M:%S")
 
         status.write(st.session_state['start_time'] + " capturing " + str(st.session_state.cap))
+
+        
 
     ##obj = st.sidebar.checkbox('Capture')
     
